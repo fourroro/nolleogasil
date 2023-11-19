580172c88c9acec2b61a5df892f8451a2818912e
MinJeong-Jang <zena4649@gmail.com>
2023-11-19 13:23:51 +0900

    first commit

---
+++ src/main/java/com/fourroro/nolleogasil/NolleogasilApplication.java
@@ -0,0 +1,13 @@
+package com.fourroro.nolleogasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolleogasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolleogasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourroro/nolleogasil/NolleogasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourroro.nolleogasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolleogasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
