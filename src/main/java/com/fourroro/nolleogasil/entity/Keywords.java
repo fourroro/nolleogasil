2a33a883063bd89b211660fa59853f3b36bf33d3
smjun0526 <smjun0526@naver.com>
2023-11-26 12:36:09 +0900

    entity, repository sunmin

---
+++ src/main/java/com/fourroro/nolleogasil/entity/Keywords.java
@@ -0,0 +1,20 @@
+package com.fourroro.nolleogasil.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="Keywords")
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자
+@AllArgsConstructor //전체 변수 생성하는 생성자
+@Embeddable
+public class Keywords implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long word_id;           //PK
+    private String keyword;         //키워드
+}
--- src/main/java/com/fourroro/nolleogasil/entity/TravelPath.java
+++ src/main/java/com/fourroro/nolleogasil/entity/TravelPath.java
@@ -1,4 +1,41 @@
 package com.fourroro.nolleogasil.entity;
 
-public class TravelPath {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name="TravelPath")
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자
+@AllArgsConstructor //전체 변수 생성하는 생성자
+@Embeddable
+public class TravelPath implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long path_id;                 //PK
+    private String departure;             //출발지
+    private String arrival;               //도착지
+    @Temporal(TemporalType.DATE)          //오라클 - timestamp 사용 (필요할 시 변경)
+    private Date startDate;               //여행 시작날짜
+    @Temporal(TemporalType.DATE)          //오라클 - timestamp 사용 (필요할 시 변경)
+    private Date endDate;		          //여행 마지막날짜
+    private String result;                //검색 결과
+
+    @ManyToOne
+    @JoinColumn(name="word_id")
+    @Embedded
+    private Keywords keywords;             //검색한 키워드 (를 이용한 타이틀도 좋을 듯)
+
+    @ManyToOne
+    @JoinColumn(name="u_id")
+    private Users user;    		          //사용자_id
+//    @Embedded
+//    private Answer answer;     	      //검색 결과
+
 }
