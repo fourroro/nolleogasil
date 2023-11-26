68e0fe9298cd73387d9437979606ad76f8f7fdb7
MinJeong-Jang <zena4649@gmail.com>
2023-11-26 12:33:38 +0900

    users

--- src/main/java/com/fourroro/nolleogasil/service/UsersService.java
+++ src/main/java/com/fourroro/nolleogasil/service/UsersService.java
@@ -1,4 +1,18 @@
 package com.fourroro.nolleogasil.service;
 
+import com.fourroro.nolleogasil.entity.Users;
+
+import java.util.List;
+
 public interface UsersService {
+    public Users findById(long u_id);
+
+    public List<Users> findAll();
+
+    //회원저장
+
+    //회원정보 수정
+    public void updateUsers(Users users);
+
+    public void deleteUsers(long u_id);
 }
